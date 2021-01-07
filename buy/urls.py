75cab3e3f9056f2fb026702ceb28a70ac7e18906
gabriel <darricson@gmail.com>
2021-01-06 23:17:46 -0300

    craiçao do banco, execuçao das migrates

--- buy/urls.py
+++ buy/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+# permite buscar dados do nosso sttings
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+
+    path('', include('produto.urls')),
     path('admin/', admin.site.urls),
-]
+
+    #  configuração necessaria para apresentação de imagens
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
